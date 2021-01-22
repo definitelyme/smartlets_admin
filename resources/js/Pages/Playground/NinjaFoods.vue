3f7e2bbc63bef8bb60776f1be56703edf7b09540
Brendan E <f0rx@noreply.github.com>
2021-01-22 20:53:41 +0200

    Init project

---
+++ resources/js/Pages/Playground/NinjaFoods.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <dropdown>Home</dropdown>
+        <secondary-button>tis is a button</secondary-button>
+    </div>
+</template>
+
+<script>
+import Dropdown from '../../Jetstream/Dropdown.vue'
+import SecondaryButton from '../../Jetstream/SecondaryButton.vue'
+export default {
+  components: { SecondaryButton, Dropdown },
+    data() {
+        return {
+            //
+        }
+    },
+}
+</script>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+Route::get('/ninjafood', function() {
+    return Inertia::render('Playground/NinjaFoods');
+});
